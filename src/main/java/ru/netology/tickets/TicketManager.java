351555f82f06000badb7d8b24ef4823a070d6154
Sergey Petrenko <Stevo98@mail.ru>
2022-12-30 11:00:12 +1000

    third commit

--- src/main/java/ru/netology/tickets/TicketManager.java
+++ src/main/java/ru/netology/tickets/TicketManager.java
@@ -4,39 +4,39 @@ import java.util.Arrays;
 
 public class TicketManager {
 
-private TicketRepository repository;
+    private TicketRepository repository;
 
-public TicketManager(TicketRepository repository) {
-    this.repository = repository;
-}
-
-public void add(Tickets product) {
-    repository.add(product);
-}
-
-public Tickets[] findAll(String from, String to) {
-    Tickets[] result = new Tickets[0];
-    for (Tickets ticket : repository.findAll()) {
-    if (matches(ticket,from,to)) {
-        Tickets[] tmp = new Tickets[result.length + 1];
-        for (int i =0; i < result.length; i++ ) {
-            tmp[i] = result[i];
-        }
-        tmp[result.length] = ticket;
-        result = tmp;
-
+    public TicketManager(TicketRepository repository) {
+        this.repository = repository;
     }
-}
-    Arrays.sort(result);
-    return result;
-}
 
-private boolean matches(Tickets ticket, String from, String to) {
-    if (ticket.getFrom().equals(from)) {
-        if (ticket.getTo().equals(to)) {
-            return true;
-        }
+    public void add(Tickets product) {
+        repository.add(product);
     }
-    return false;
-}
+
+    public Tickets[] findAll(String from, String to) {
+        Tickets[] result = new Tickets[0];
+        for (Tickets ticket : repository.findAll()) {
+            if (matches(ticket, from, to)) {
+                Tickets[] tmp = new Tickets[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[result.length] = ticket;
+                result = tmp;
+
+            }
+        }
+        Arrays.sort(result);
+        return result;
+    }
+
+    private boolean matches(Tickets ticket, String from, String to) {
+        if (ticket.getFrom().equals(from)) {
+            if (ticket.getTo().equals(to)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/main/java/ru/netology/tickets/TicketRepository.java
+++ src/main/java/ru/netology/tickets/TicketRepository.java
@@ -2,18 +2,18 @@ package ru.netology.tickets;
 
 public class TicketRepository {
 
-private Tickets[] tickets = new Tickets[0];
+    private Tickets[] tickets = new Tickets[0];
 
-public Tickets[] findAll() {
-    return tickets;
-}
-
-public void add(Tickets ticket) {
-    Tickets[] tmp = new Tickets[tickets.length + 1];
-    for (int i = 0; i < tickets.length; i++) {
-        tmp[i] = tickets[i];
+    public Tickets[] findAll() {
+        return tickets;
     }
-    tmp[tmp.length -1 ] = ticket;
-    tickets = tmp;
-}
+
+    public void add(Tickets ticket) {
+        Tickets[] tmp = new Tickets[tickets.length + 1];
+        for (int i = 0; i < tickets.length; i++) {
+            tmp[i] = tickets[i];
+        }
+        tmp[tmp.length - 1] = ticket;
+        tickets = tmp;
+    }
 }
